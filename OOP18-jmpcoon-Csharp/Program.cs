7dbb41a9fb6847bb8d8c065f3a0b236abd87a57f
cake_lier <matteo.castellucci14@hotmail.it>
2019-05-03 00:52:29 +0200

    Implement first version of ClassToInstanceMultimap

---
+++ OOP18-jmpcoon-Csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP18jmpcoonCsharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            ClassToInstanceMultimap<int> numbers = new ClassToInstanceMultimap<int>();
+        }
+    }
+}
